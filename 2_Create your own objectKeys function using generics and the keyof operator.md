60379c913ccccfaf2f0d3a4de8d940cb3f554784
wowdyaln <rogersmk@gmail.com>
2023-10-15 00:13:25 +0800

    init

---
+++ 2_Create your own objectKeys function using generics and the keyof operator.md
@@ -0,0 +1,4 @@
+非常重要的觀念：
+- [Create your own 'objectKeys' function using generics and the 'keyof' operator | Total TypeScript](https://www.totaltypescript.com/tips/create-your-own-objectkeys-function-using-generics-and-the-keyof-operator)
+
+
--- tip1_Derive a union type from an object.ts
+++ tip1_Derive a union type from an object.ts
@@ -11,7 +11,7 @@ I start with some code that looks like this
 
 */
 
-export const fruitCount = {
+export const fruitCounts = {
   apple: 1,
   pear: 4,
   banana: 26,
@@ -41,22 +41,26 @@ then I'm going to make a new type called NewSingleFruitCount.
 
 */
 
-// type FruitCounts = typeof fruitCounts type NewSingleFruitCount = { [K in keyof FruitCounts]: {} }
-// type FruitCounts = typeof fruitCounts
+type FruitCounts = typeof fruitCounts
 
-// type NewSingleFruitCount = {
-//   [K in keyof FruitCounts]: {}
-// }
+type NewSingleFruitCount = {
+  [K in keyof FruitCounts]: {
+    [K2 in K]: number
+  }
+}[keyof FruitCounts]
+
+const fruit: NewSingleFruitCount ={ apple: 3}
 
 /*
 
 
-What this is doing is its taking each key in FruitCounts and setting its type to an empty object. So when I use it, TypeScript will be expecting a shape like this.
+
+What this is doing is its taking each key in FruitCounts and setting its type to an empty object.
+So when I use it, TypeScript will be expecting a shape like this.
 
 */
 
 
-// const singleFruitCount: NewSingleFruitCount = { apple: {}, pear: {}, banana: {}, }
 // const singleFruitCount: NewSingleFruitCount = {
 //   apple: {},
 //   pear: {},
@@ -68,10 +72,8 @@ What this is doing is its taking each key in FruitCounts and setting its type to
 
 But, I want the properties to be numbers.
 
-
 */
 
-// type NewSingleFruitCount = { [K in keyof FruitCounts]: { [K2 in K]: number } }
 // type NewSingleFruitCount = {
 //   [K in keyof FruitCounts]: {
 //     [K2 in K]: number
@@ -81,12 +83,12 @@ But, I want the properties to be numbers.
 
 /*
 
-As you can see, if I just do that, I still haven't quite got a union type. I get this odd nested structure, and I will still have to have every possible fruit present.
+As you can see, if I just do that, I still haven't quite got a union type.
+I get this odd nested structure, and I will still have to have every possible fruit present.
 
 
 */
 
-// const singleFruitCount: NewSingleFruitCount = { apple: { apple: 2, }, pear: { pear: 4, }, banana: { banana: 26, }, }
 // const singleFruitCount: NewSingleFruitCount = {
 //   apple: {
 //     apple: 2,
@@ -102,12 +104,12 @@ As you can see, if I just do that, I still haven't quite got a union type. I get
 
 /*
 
-So, what I'm going to do is map over our NewSingleFruitCount type to get rid of those parent keys, and fully create my union type.
+So, what I'm going to do is map over our NewSingleFruitCount type to get rid of those parent keys,
+and fully create my union type.
 
 
 */
 
-// type NewSingleFruitCount = { [K in keyof FruitCounts]: { [K2 in K]: number } }[keyof FruitCounts]
 // type NewSingleFruitCount = {
 //   [K in keyof FruitCounts]: {
 //     [K2 in K]: number
@@ -122,7 +124,6 @@ And with that, I can now assign our singleFruitCount how I'd like.
 
 */
 
-// const singleFruitCount: NewSingleFruitCount = { apple: 2, }
 // const singleFruitCount: NewSingleFruitCount = {
 //   apple: 2,
 // }
